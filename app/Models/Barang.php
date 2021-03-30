47ecb2aed72b8f3a8f1045c10fc98caac7feeefe
Tita Wijayanti <wijayantitita094@gmail.com>
2021-03-30 15:53:08 +0700

    Setting $fillable Model Barang berdasarkan data yang ingin dapat ditambah atau diedit pada kasus ini id_barang, kode_barang, nama_barang, kategori_barang, harga, qty

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -7,5 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Barang extends Model
 {
-    use HasFactory;
+    protected $table="barang"; // Eloquent akan membuat model barang menyimpan record di tabel barangs
+    public $timestamps= false;
+    protected $primaryKey = 'id_barang'; // Memanggil isi DB Dengan primarykey
+    /**
+     * The attributes that are mass assignable. *
+     * @var array
+     */
+    protected $fillable = [
+        'id_barang',
+        'kode_barang',
+        'nama_barang',
+        'kategori_barang',
+        'harga',
+        'qty',
+    ];
 }
