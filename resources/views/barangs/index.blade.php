fa907b196bcff2cb57665f3df48d54e253a93fc2
Tita Wijayanti <wijayantitita094@gmail.com>
2021-03-30 16:00:25 +0700

    Mengisi kode program pada index.blade.php

---
+++ resources/views/barangs/index.blade.php
@@ -0,0 +1,52 @@
+@extends('barangs.layout')
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left mt-2">
+                <h2>Data Barang</h2>
+            </div>
+
+            <div class="float-right my-2">
+                <a class="btn btn-success" href="{{ route('barang.create') }}"> Input Barang</a>
+            </div>
+        </div>
+    </div>
+
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+
+    <table class="table table-bordered">
+        <tr>
+            <th>id_barang</th>
+            <th>kode_barang</th>
+            <th>nama_barang</th>
+            <th>kategori_barang</th>
+            <th>harga</th>
+            <th>qty</th>
+            <th width="280px">Action</th>
+        </tr>
+        @foreach ($barangs as $Barang)
+            <tr>
+
+                <td>{{ $Barang->id_barang }}</td>
+                <td>{{ $Barang->kode_barang }}</td>
+                <td>{{ $Barang->nama_barang }}</td>
+                <td>{{ $Barang->kategori_barang }}</td>
+                <td>{{ $Barang->harga }}</td>
+                <td>{{ $Barang->qty }}</td>
+                <td>
+                    <form action="{{ route('barang.destroy', $barang->id_barang) }}" method="POST">
+
+                        <a class="btn btn-info" href="{{ route('barang.show', $barang->barang) }}">Show</a>
+                        <a class="btn btn-primary" href="{{ route('barang.edit', $barang->Nim) }}">Edit</a>
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+    </table>
+@endsection
